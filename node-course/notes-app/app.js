971af3cab1e2715d992604ae00229931a65f5a56
meghalmodi12 <meghalmodi12@gmail.com>
2020-05-14 23:53:03 -0400

    Added code for notes app

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,3 +1,45 @@
-require('./utils');
+const chalk = require('chalk');
+const yargs = require('yargs');
+const notes = require('./notes');
 
-const fs = require('fs');
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log(`Title : ${argv.title}`);
+        console.log(`Body : ${argv.body}`);
+    }
+});
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log(chalk.red('Remove a note'));
+    }
+});
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function() {
+        console.log(chalk.blue('List all notes'));
+    }
+});
+
+yargs.parse();
