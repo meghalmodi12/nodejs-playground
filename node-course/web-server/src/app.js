6cf4ebc6f28c1d5480f1de663e0736c48afc23f7
meghalmodi12 <meghalmodi12@gmail.com>
2020-05-27 22:43:37 -0400

    Added static files and HBS views

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -1,20 +1,34 @@
 require('dotenv').config();
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.get('/', (req, res) => {
-    res.send('Hello Express!!');
-})
+app.set('view engine', 'hbs');
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        pageTitle: 'Home'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        pageTitle: 'About',
+        imgPath: '/img/profile.png'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
         location: 'Edison',
         condition: 'Sunny',
         temprature: 23
-    })
-})
+    });
+});
 
 app.listen(port, () => {
     console.log(`Express app running on port ${port}.`)
