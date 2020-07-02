8b5dcb086faeca0b0ada7db8837b3fbb8b0542e8
meghalmodi12 <meghalmodi12@gmail.com>
2020-07-02 15:16:01 -0400

    Added logout feature for task-manager app

--- node-course/task-manager/src/routers/user.js
+++ node-course/task-manager/src/routers/user.js
@@ -25,6 +25,26 @@ router.post('/users/login', async (req, res) => {
     }
 });
 
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter(token => token.token !== req.token);
+        await req.user.save();
+        res.status(200).send(req.user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+        res.status(200).send(req.user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user);
 });
