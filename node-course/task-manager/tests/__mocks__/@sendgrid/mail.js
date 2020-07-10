5b8ab87012c00e75af151a9f18c6746a28f2a018
meghalmodi12 <meghalmodi12@gmail.com>
2020-07-10 18:48:44 -0400

    Mocking npm modules - avoid sending actual emails

---
+++ node-course/task-manager/tests/__mocks__/@sendgrid/mail.js
@@ -0,0 +1,8 @@
+module.exports = {
+    setApiKey() {
+        console.log('Mocking @sendgrid/mail setApiKey function');
+    },
+    send() {
+        console.log('Mocking @sendgrid/mail send function');
+    }
+};
