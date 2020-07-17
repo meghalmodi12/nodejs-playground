a3324af7537e6bdced242094e2bbe27971a45cab
meghalmodi12 <meghalmodi12@gmail.com>
2020-07-17 15:08:42 -0400

    Rendering messages using mustache.js

--- node-course/chat-app/public/js/chat.js
+++ node-course/chat-app/public/js/chat.js
@@ -1,13 +1,32 @@
 const socket = io();
 
 // Elements
+const $messages = document.querySelector('#divMessages');
 const $messageForm = document.querySelector('#message-form');
 const $messageFormInput = document.querySelector('#txtMessage');
 const $messageFormButton = $messageForm.querySelector('button');
 const $locationBtn = document.querySelector('#btnSendLoc');
 
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMessageTemplate = document.querySelector('#locationMessage-template').innerHTML;
+
 socket.on('message', (message) => {
     console.log(message);
+
+    const html = Mustache.render(messageTemplate, {
+        message
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
+}); 
+
+socket.on('locationMessage', (locationMessage) => {
+    console.log(locationMessage);
+
+    const html = Mustache.render(locationMessageTemplate, {
+        locationMessage
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
 });
 
 $messageForm.addEventListener('submit', (event) => {
